ead2d47dd827f183d267244223e8bd177828613c
drsamitha <49331487+drsamitha@users.noreply.github.com>
2023-07-09 22:40:06 +0530

    Add only encryption feature
    Need to change user inputs and need to remove the formula

--- patient_record.cpp
+++ patient_record.cpp
@@ -1,26 +1,55 @@
+#include <string>
+#include <sstream>
 #include <iostream>
 #include <fstream>
 using namespace std;
+
+string encrypt(string data) {
+    string encryptedData = "";
+    int key = 3; // Encryption key
+    for (int i = 0; i < data.length(); i++) {
+        char c = data[i];
+        if (isalpha(c)) {
+            char offset = isupper(c) ? 'A' : 'a';
+            c = (c - offset + key) % 26 + offset;
+        }
+        encryptedData += c;
+    }
+    return encryptedData;
+}
 
 int main() {
     string username;
     double weight, age, time;
 
     // Input weight, username, and age
-    cout << "Enter weight: ";
+    cout << "Name of the patient: ";
+    getline(cin, username);
+
+    cout << "Weight of the patient: ";
     cin >> weight;
-    cout << "Enter username: ";
-    cin >> username;
-    cout << "Enter age: ";
+
+    cout << "Enter patient's age: ";
     cin >> age;
 
     // Calculate inject time
     time = weight / age;
+    cout << "Time duration is " << time << endl;
 
-    // Store the record in "DB.txt" file
+// Convert time to string
+stringstream ss;
+ss << time;
+string timeStr = ss.str();
+
+// Encrypt data
+string data = "Username: " + username + ", Time: " + timeStr;
+string encryptedData = encrypt(data);
+
+
+    // Store the encrypted record in "DB.txt" file
     ofstream outputFile("DB.txt", ios::app);
     if (outputFile.is_open()) {
-        outputFile << "Username: " << username << ", Time: " << time << endl;
+        outputFile << encryptedData << endl;
         outputFile.close();
         cout << "Record saved successfully!" << endl;
     } else {
@@ -29,3 +58,29 @@ int main() {
 
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
